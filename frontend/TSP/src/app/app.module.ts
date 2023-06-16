fa8eac5775b45edbc6b8c57e1c2c1fe61bf60a07
Dilini Senarathna <112084366+senarathnasaddd@users.noreply.github.com>
2023-06-16 11:02:49 +0530

    add confirm booking

--- frontend/TSP/src/app/app.module.ts
+++ frontend/TSP/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ConfirmBookingComponent } from './confirm-booking/confirm-booking.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    ConfirmBookingComponent
   ],
   imports: [
     BrowserModule,
